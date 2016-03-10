93fee350777fc50ccf0a989894a478b370a036c9
Maira Bello <maira.araujo@liferay.com>
2016-03-10 13:15:56 -0300

    Wraps the file in a `goog.loadModule` call if a `goog.module` call is found

--- index.js
+++ index.js
@@ -1,8 +1,77 @@
 'use strict';
 
-module.exports = function() {
+module.exports = function(babel) {
+  var t = babel.types;
+
+  /**
+   * Checks if the given member expression is accessing `goog.module`.
+   * @param {!MemberExpression} member
+   * @return boolean
+   */
+  function isGoogModuleCall(member) {
+    return member.object.name === 'goog' && member.property.name === 'module';
+  }
+
   return {
     visitor: {
+      /**
+       * Checks if the call expression is `goog.module`.
+       * @param {!NodePath} path
+       * @param {!Object} state
+       */
+      CallExpression: function(path, state) {
+        var callee = path.node.callee;
+        if (t.isMemberExpression(callee) && isGoogModuleCall(callee)) {
+          state.foundGoogModuleCall = true;
+        }
+      },
+
+      MemberExpression: function(path, state) {
+        if (!state.foundGoogModuleCall) {
+          return;
+        }
+
+        if (path.node.object.name === 'exports') {
+          path.replaceWith(
+            t.memberExpression(
+              t.identifier('closureExports'),
+              path.node.property,
+              path.node.computed
+            )
+          );
+        }
+      },
+
+      Program: {
+        /**
+         * Wraps the program body in a `goog.loadModule` call, if a
+         * `goog.module` call was detected inside it.
+         * @param {!NodePath} path
+         * @param {!Object} state
+         */
+        exit: function(path, state) {
+          if (!state.foundGoogModuleCall) {
+            return;
+          }
+
+          var contents = path.node.body;
+          contents.push(
+            t.returnStatement(t.identifier('closureExports'))
+          );
+          path.node.body = [t.expressionStatement(t.callExpression(
+            t.memberExpression(
+              t.identifier('goog'),
+              t.identifier('loadModule'),
+              false
+            ),
+            [t.functionExpression(
+              null,
+              [t.identifier('closureExports')],
+              t.blockStatement(contents)
+            )]
+          ))];
+        }
+      }
     }
   };
 };
--- test/test.js
+++ test/test.js
@@ -2,7 +2,37 @@
 
 var assert = require('assert');
 var babel = require('babel-core');
-var closureModulePlugin = require('../index');
+var plugin = require('../index');
 
 module.exports = {
+  testWrapModuleIfGoogModuleCall: function(test) {
+    var code = 'goog.module("abc");';
+    var result = babel.transform(code, {plugins: [plugin]});
+    var expectedResult = 'goog.loadModule(function (closureExports) {\n' +
+      '  goog.module("abc");\n' +
+      '  return closureExports;\n' +
+      '});';
+    assert.strictEqual(expectedResult, result.code);
+    test.done();
+  },
+
+  testWrapModuleAndRenameExportsIfGoogModuleCall: function(test) {
+    var code = 'goog.module("abc");\nexports.a = 1;';
+    var result = babel.transform(code, {plugins: [plugin]});
+    var expectedResult = 'goog.loadModule(function (closureExports) {\n' +
+      '  goog.module("abc");\n' +
+      '  closureExports.a = 1;\n' +
+      '  return closureExports;\n' +
+      '});';
+    assert.strictEqual(expectedResult, result.code);
+    test.done();
+  },
+
+  testDoNothingIfNoGoogModuleCall: function(test) {
+    var code = 'a("abc");\nexports.a = 1;';
+    var result = babel.transform(code, {plugins: [plugin]});
+    var expectedResult = 'a("abc");\nexports.a = 1;';
+    assert.strictEqual(expectedResult, result.code);
+    test.done();
+  }
 };
